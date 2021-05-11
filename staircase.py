7bf53775499314aba5cd100f16e3a7a72f7bbffa
Oshal Borkar <oshalb08@gmail.com>
2021-05-11 16:52:28 +0530

    Staircase

---
+++ staircase.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input().strip())
+    for i in range(1, n+1):
+        for j in range(i+1, n+1):
+            print(' ',end='')
+        c = '#'*i
+        print(c)
