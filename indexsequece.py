64eefe58d5171299d1e59c67382ef9a4d59eb118
Oshal Borkar <oshalb08@gmail.com>
2021-06-09 17:27:11 +0530

    Sequence Equation

---
+++ indexsequece.py
@@ -0,0 +1,7 @@
+# Sequence Equation
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    p = list(map(int, input().strip().split(' ')))
+    for i in range(n):
+        print(p.index(p.index(i + 1) + 1) + 1)
