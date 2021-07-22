ff593f4e82552d5fb585ff628d94cb4ef228caed
Oshal Borkar <oshalb08@gmail.com>
2021-07-22 11:09:18 +0530

    Reverse a doubly linked list

--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -49,6 +49,20 @@ class DoublyLinkedList:
                 else:
                     t.next = node
 
+    def reverse_list(self):
+        t = self.head
+        while t.next:
+            t = t.next
+        node = Node(t.data)
+        self.head = node
+        self.head.next = t.prev
+        temp = self.head.next
+        while temp:
+            f = temp.next
+            temp.next = temp.prev
+            temp.prev = f
+            temp = temp.next
+
     def print_list(self):
         if self.head:
             n = self.head
@@ -63,9 +77,22 @@ if __name__ == "__main__":
     dllist = DoublyLinkedList()
     number_of_testcases = int(input())
     for _ in range(number_of_testcases):
+        print("Enter Length of List")
         length_of_list = int(input())
+        print("Enter values into list, seperate by enter")
         for i in range(length_of_list):
             dllist.add_node(int(input()))
-        value_to_be_inserted = int(input())
-        dllist.insert_into_sorted_list(value_to_be_inserted)
-        dllist.print_list()
+        print("Select Options", "0. Exit", "1. Insert", "2. Reverse", "3. Print", sep="\n")
+        s = int(input())
+        while s != 0:
+            if s == 1:
+                value_to_be_inserted = int(input())
+                dllist.insert_into_sorted_list(value_to_be_inserted)
+            elif s == 2:
+                dllist.reverse_list()
+            elif s == 3:
+                dllist.print_list()
+            else:
+                print("String does not match")
+            print("Select Options", "0. Exit", "1. Insert", "2. Reverse", "3. Print", sep="\n")
+            s = int(input())
